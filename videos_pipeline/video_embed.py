90d53e1aab11cc463267ce037cd5de8219c0924f
hetkpatel <hp145371@gmail.com>
2024-01-10 04:11:38 -0600

    Update video_embed.py

--- videos_pipeline/video_embed.py
+++ videos_pipeline/video_embed.py
@@ -3,48 +3,109 @@ import cv2
 from PIL import Image
 from os import path, makedirs
 from mimetypes import guess_type
-from tqdm import tqdm, trange
+from multiprocessing import Manager, cpu_count
+from concurrent.futures import ProcessPoolExecutor
 from uuid import uuid4
 from json import load, dump
 
 import embedding_models.ResNet50_Embedding as ie
 
+from rich import progress as P
+
+
+def _embed(session, id, file, progress, task_id):
+    model = ie.ResNet50_ImageEmbedder()
+    model.eval()
+    with torch.no_grad():
+        # Create video feed from file
+        video = cv2.VideoCapture(file)
+        for n in range(int(video.get(cv2.CAP_PROP_FRAME_COUNT))):
+            # Get frame
+            success, frame = video.read()
+            if success:
+                # Transform images into tensors
+                t = ie.get_transforms()(Image.fromarray(frame))
+                # Create embedding vector
+                vector = torch.squeeze(model(t.unsqueeze(0)))
+                # Save vector in tmp session folder
+                if not path.exists(f"./.tmp/{session}/v/vectors/{id}"):
+                    makedirs(f"./.tmp/{session}/v/vectors/{id}")
+                torch.save(vector, f"./.tmp/{session}/v/vectors/{id}/{uuid4().hex}.pt")
+
+            # Update progress
+            progress[task_id] = {
+                "progress": n + 1,
+                "total": int(video.get(cv2.CAP_PROP_FRAME_COUNT)),
+            }
+
+        # Release video memory
+        video.release()
+
 
 def process(session, input):
     try:
-        model = ie.ResNet50_ImageEmbedder()
-
-        file_list = []
-        for i in input:
-            file_list += _validate_source(i)
-
         if not path.exists(f"./.tmp/{session}/v/vectors"):
             makedirs(f"./.tmp/{session}/v/vectors")
 
-        model.eval()
-        with torch.no_grad():
-            for file in tqdm(desc="Indexing video vector database", iterable=file_list):
-                # Create video feed from file
-                video = cv2.VideoCapture(file)
-                _, frame = video.read()
-                id = uuid4().hex
-                # Save id and file path in tmp session folder in json file
-                _save_id_to_file(session, id, path.abspath(file))
-                for _ in trange(int(video.get(cv2.CAP_PROP_FRAME_COUNT))):
-                    # Transform images into tensors
-                    t = ie.get_transforms()(Image.fromarray(frame))
-                    # Create embedding vector
-                    vector = torch.squeeze(model(t.unsqueeze(0)))
-                    # Save vector in tmp session folder
-                    if not path.exists(f"./.tmp/{session}/v/vectors/{id}"):
-                        makedirs(f"./.tmp/{session}/v/vectors/{id}")
-                    torch.save(
-                        vector, f"./.tmp/{session}/v/vectors/{id}/{uuid4().hex}.pt"
-                    )
-                    # Get next frame
-                    _, frame = video.read()
+        with P.Progress(
+            "[progress.description]{task.description}",
+            P.BarColumn(bar_width=None),
+            P.TaskProgressColumn(show_speed=True),
+            P.MofNCompleteColumn(),
+            P.TimeRemainingColumn(),
+            expand=True,
+        ) as progress:
+            futures = []  # keep track of the jobs
+            with Manager() as manager:
+                # this is the key - we share some state between our
+                # main process and our worker functions
+                _progress = manager.dict()
+                overall_progress_task = progress.add_task("[green]All jobs progress:")
 
-                video.release()
+                with ProcessPoolExecutor(max_workers=cpu_count() * 2) as executor:
+                    for file in [f for f in input if _validate_source(f)]:
+                        task_id = progress.add_task(
+                            f"Processing {path.basename(file)}...", visible=True
+                        )
+                        id = uuid4().hex
+                        # Save id and file path in tmp session folder in json file
+                        _save_id_to_file(session, id, path.abspath(file))
+                        futures.append(
+                            executor.submit(
+                                _embed,  # function
+                                session,  # session
+                                id,  # id
+                                file,  # file
+                                _progress,
+                                task_id,
+                            )
+                        )
+
+                    # monitor the progress:
+                    while (
+                        n_finished := sum([future.done() for future in futures])
+                    ) < len(futures):
+                        progress.update(
+                            overall_progress_task,
+                            completed=n_finished,
+                            total=len(futures),
+                        )
+                        for task_id, update_data in _progress.items():
+                            latest = update_data["progress"]
+                            total = update_data["total"]
+                            # update the progress bar for this task:
+                            progress.update(
+                                task_id,
+                                completed=latest,
+                                total=total,
+                                visible=latest < total,
+                            )
+
+                    progress.update(overall_progress_task, advance=1)
+
+                    # raise any errors:
+                    for future in futures:
+                        future.result()
 
     except Exception as e:
         raise e
@@ -52,12 +113,9 @@ def process(session, input):
 
 def _validate_source(source):
     try:
-        if guess_type(source)[0].startswith("video/"):
-            return [source]
+        return guess_type(source)[0].startswith("video/")
     except:
-        pass
-
-    return []
+        return False
 
 
 def _save_id_to_file(session, id, filename):
